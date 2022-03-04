6ad986d00fe9f6a5e49f2418d942016857a37fc1
Tianpeng Wang <tpwang@alauda.io>
2022-03-04 22:16:54 +0800

    add covercheck config to check cmd
    Signed-off-by: Tianpeng Wang <tpwang@alauda.io>

--- cmd/read.go
+++ cmd/read.go
@@ -15,21 +15,23 @@ var readCmd = &cobra.Command{
 	Use:    "read",
 	Short:  "Read coverage data",
 	PreRun: prerunBindViperFlags,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		tool, err := covertool.New(
-			viper.GetString(constants.APIBase), viper.GetString(constants.APIToken), viper.GetString(constants.ProjectID))
-		if err != nil {
-			return fmt.Errorf("failed to initialize covertool: %w", err)
-		}
+	RunE:   runRead,
+}
 
-		coverage, err := tool.Read(cmd.Context(), viper.GetString(constants.PipelineName), viper.GetString(constants.GitRef))
-		if err != nil {
-			return err
-		}
+func runRead(cmd *cobra.Command, args []string) error {
+	tool, err := covertool.New(
+		viper.GetString(constants.APIBase), viper.GetString(constants.APIToken), viper.GetString(constants.ProjectID))
+	if err != nil {
+		return fmt.Errorf("failed to initialize covertool: %w", err)
+	}
 
-		fmt.Printf("%.2f\n", coverage.ValueOrZero())
-		return nil
-	},
+	coverage, err := tool.Read(cmd.Context(), viper.GetString(constants.PipelineName), viper.GetString(constants.GitRef))
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%.2f\n", coverage.ValueOrZero())
+	return nil
 }
 
 func init() {
--- cmd/write.go
+++ cmd/write.go
@@ -18,21 +18,23 @@ var writeCmd = &cobra.Command{
 	Short:  "Write coverage data",
 	PreRun: prerunBindViperFlags,
 	Args:   cobra.ExactArgs(1),
-	RunE: func(cmd *cobra.Command, args []string) error {
-		cover, err := covertool.New(
-			viper.GetString(constants.APIBase), viper.GetString(constants.APIToken), viper.GetString(constants.ProjectID))
-		if err != nil {
-			return fmt.Errorf("failed to initialize covertool: %w", err)
-		}
+	RunE:   runWrite,
+}
 
-		coverage, err := strconv.ParseFloat(args[0], 64)
-		if err != nil {
-			return errors.New("coverage must in float")
-		}
+func runWrite(cmd *cobra.Command, args []string) error {
+	cover, err := covertool.New(
+		viper.GetString(constants.APIBase), viper.GetString(constants.APIToken), viper.GetString(constants.ProjectID))
+	if err != nil {
+		return fmt.Errorf("failed to initialize covertool: %w", err)
+	}
 
-		return cover.Write(cmd.Context(),
-			viper.GetString(constants.PipelineName), viper.GetString(constants.GitRef), viper.GetString(constants.GitSHA), coverage)
-	},
+	coverage, err := strconv.ParseFloat(args[0], 64)
+	if err != nil {
+		return errors.New("coverage must in float")
+	}
+
+	return cover.Write(cmd.Context(),
+		viper.GetString(constants.PipelineName), viper.GetString(constants.GitRef), viper.GetString(constants.GitSHA), coverage)
 }
 
 func init() {
